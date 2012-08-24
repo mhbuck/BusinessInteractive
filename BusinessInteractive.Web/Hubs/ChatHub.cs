a3e7c27a9ed231c072a2024bbdd70f1cb0bce4de
Michael Buck <mhbuck@gmail.com>
2012-08-24 12:27:30 +0200

    Create a start viewing method and send message

--- BusinessInteractive.Web/Hubs/ChatHub.cs
+++ BusinessInteractive.Web/Hubs/ChatHub.cs
@@ -9,9 +9,16 @@ namespace BusinessInteractive.Web.Hubs
     public class ChatHub : Hub
     {
         public void Send(string orderId, string message)
-        { 
-           Clients.
+        {
+            //Send messages out to the people who are on the same order
+            Clients[orderId].addMessage(message);
         }
 
+        public void StartViewingOrder(string orderId)
+        {
+            this.Groups.Add(this.Context.ConnectionId, orderId);
+
+            Clients[orderId].newPersonJoined(this.Context.ConnectionId);
+        }
     }
 }
--- BusinessInteractive.Web/Scripts/Detail.js
+++ BusinessInteractive.Web/Scripts/Detail.js
@@ -1,1 +1,30 @@
-﻿
+﻿$(function () {
+	// Proxy created on the fly
+	var chat = $.connection.chatHub;
+
+	
+	// Declare a function on the chat hub so the server can invoke it
+	chat.addMessage = function (message) {
+		$('#messages').append('<li>' + message + '</li>');
+	};
+
+	chat.newPersonJoined = function (personJoined)
+	{
+		if (personJoined !== $.connection.hub.id) {
+			alert(personJoined);
+		}
+	}
+
+	/*
+	$("#broadcast").click(function () {
+		// Call the chat method on the server
+		chat.send($('#msg').val());
+	});
+	*/
+	// Start the connection
+	$.connection.hub.start(
+		function () {
+			var orderId = $('#OrderId').val();
+			chat.startViewingOrder(orderId);
+		});
+});
