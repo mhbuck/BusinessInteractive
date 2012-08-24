9b653f6dee280af56cae22cf6c0b53f1d51e057d
Michael Buck <mhbuck@gmail.com>
2012-08-24 11:56:21 +0200

    Add the chat hub that is going to be used

---
+++ BusinessInteractive.Web/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace BusinessInteractive.Web.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string orderId, string message)
+        { 
+           Clients.
+        }
+
+    }
+}
